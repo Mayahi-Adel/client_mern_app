51cfb2f55d7fda43acf7765b84f7d5f7d131f0dd
Mayahi Adel <adel.mayahii@gmail.com>
2022-01-10 11:06:51 +0100

    post error msg

--- src/actions/post.actions.js
+++ src/actions/post.actions.js
@@ -54,7 +54,7 @@ export const addPost = (data) => {
         dispatch({ type: GET_POST_ERRORS, payload: "" });
       }
     } catch (err) {
-      console.log(err.message);
+      console.log("ERROR :", err);
     }
   };
 };
